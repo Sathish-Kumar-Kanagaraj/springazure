6ecc66bd37de828a94a5461862635feebf5e8055
user <user@DESKTOP-TIEP7GJ>
2022-12-07 15:45:52 +0530

    Rest API

--- azuredemo/src/main/java/com/sathish/azuredemo/AzuredemoApplication.java
+++ azuredemo/src/main/java/com/sathish/azuredemo/AzuredemoApplication.java
@@ -3,20 +3,21 @@ package com.sathish.azuredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzuredemoApplication {
 
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(AzuredemoApplication.class, args);
 	}
-	
 
 	@GetMapping("/message")
 	public String message() {
 		return "Congrats!azure deploy success";
 	}
 
+	
 }
