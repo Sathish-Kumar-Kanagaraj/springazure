fbf4e37e8e3e1d0513565ae606c706e1c4467054
user <user@DESKTOP-TIEP7GJ>
2022-12-07 15:29:29 +0530

    Adding first End point.

--- azuredemo/src/main/java/com/sathish/azuredemo/AzuredemoApplication.java
+++ azuredemo/src/main/java/com/sathish/azuredemo/AzuredemoApplication.java
@@ -2,12 +2,21 @@ package com.sathish.azuredemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class AzuredemoApplication {
 
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AzuredemoApplication.class, args);
+	}
+	
+
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats!azure deploy success";
 	}
 
 }
